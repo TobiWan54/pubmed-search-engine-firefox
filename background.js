e56d13ffcf0456ed9fa55b03479be7840e72a3d7
TobiWan54 <tobydale25@hotmail.com>
2022-11-12 23:46:47 +0000

    Linked to PubMed
    Also changed icon file types from .png to .ico

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 browser.contextMenus.create({
-    id: "search-with-google-scholar",
-    title: "Search in Google Scholar",
+    id: "search-with-pubmed",
+    title: "Search in PubMed",
     icons: {
-        "16": "icons/16.png",
-        "24": "icons/24.png",
-        "32": "icons/32.png",
-        "48": "icons/48.png",
+        "16": "icons/16.ico",
+        "24": "icons/24.ico",
+        "32": "icons/32.ico",
+        "48": "icons/48.ico",
     },
     contexts: ["selection"],
 });
 
 browser.contextMenus.onClicked.addListener(function (info, tab) {
     switch (info.menuItemId) {
-        case "search-with-google-scholar":
-            browser.tabs.create({ url: "https://scholar.google.com/scholar?q=" + info.selectionText });
+        case "search-with-pubmed":
+            browser.tabs.create({ url: "https://pubmed.ncbi.nlm.nih.gov/?term=" + info.selectionText });
             break;
     }
 })
